2d593954eadd66e4a9fa96589bf2f258b8c20f20
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-03-14 00:13:56 -0300

    Add logic to GUI scene

--- assets/scripts/gui.gd
+++ assets/scripts/gui.gd
@@ -1,15 +1,11 @@
 extends CanvasLayer
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	%Score.text = "SCORE: " + str(GLOBAL.score)
 
 
 func _on_animation_player_animation_finished(anim_name):
-	pass # Replace with function body.
+	match(anim_name):
+		"fadeout":
+			get_tree().change_scene_to_file("res://scenes/menu.tscn")
