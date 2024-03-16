da5d5ac3122913193e4330c53075edbcc115d0d3
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2024-03-16 01:13:23 -0300

    Fix logic to move player

--- assets/scripts/GLOBAL.gd
+++ assets/scripts/GLOBAL.gd
@@ -6,6 +6,6 @@ var score : int = 0
 var axis : Vector2
 
 func get_axis() -> Vector2:
-	axis.x = int(Input.is_action_just_pressed("ui_right")) - int(Input.is_action_just_pressed("ui_left"))
-	axis.y = int(Input.is_action_just_pressed("ui_up")) - int(Input.is_action_just_pressed("ui_down"))
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.y = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
 	return axis.normalized()
